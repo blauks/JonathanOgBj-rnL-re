589551f6ab102533415e3b23a04ac774d719bc68
Jonathan Brooks <brooks.jonathan1@gmail.com>
2018-02-14 13:12:23 +0100

    tok vekk en variabel

--- Læringsapplikasjon/Form1.cs
+++ Læringsapplikasjon/Form1.cs
@@ -43,28 +43,18 @@ namespace Læringsapplikasjon
         {
             Button bt = sender as Button;
 
-            bool TrueOrFalse = false;
-
             if(bt == btStartF)
             {
                 pbStartmeny.Image = Læringsapplikasjon.Properties.Resources.figurspill;
-                TrueOrFalse = true;
             }
             else if(bt == btStartT)
             {
                 pbStartmeny.Image = Læringsapplikasjon.Properties.Resources.tallspill;
-                TrueOrFalse = true;
             }
             else if(bt == btStartD)
             {
                 pbStartmeny.Image = Læringsapplikasjon.Properties.Resources.dyrspill;
-                TrueOrFalse = true;
             }
-            else
-            {
-                TrueOrFalse = false;
-            }
-            pbStartmeny.Visible = TrueOrFalse;
 
         }
 
